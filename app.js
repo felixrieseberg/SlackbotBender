2ccf163befc4f41e240c4698a15ff8badab2f32d
Felix Rieseberg <felix@felixrieseberg.com>
2014-09-25 13:44:16 -0700

    Cleanup node modules

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express'),
     path = require('path'),
     favicon = require('serve-favicon'),
     logger = require('morgan'),
-    cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
 
     bot = require('./bot/index'),
@@ -16,7 +15,6 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', bot);
