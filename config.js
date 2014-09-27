75ae925ab50c1f389f1ebe88a995aa21a09e8408
Felix Rieseberg <felix@felixrieseberg.com>
2014-09-26 17:29:04 -0700

    Ping Bugfix (II)

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var config = {
 
     slacktoken: process.env.slacktoken,
-    twilioNumber: process.env.twilioNumber || '',
-    twilioAccount: process.env.twilioAccount || '',
-    twilioToken: process.env.twilioToken || '',
+    twilioNumber: process.env.twilioNumber || '+19143686655',
+    twilioAccount: process.env.twilioAccount || 'AC187fee161f0722fdbafadb0c6e714053',
+    twilioToken: process.env.twilioToken || '80790faef92edd07e2b2d265e6eeafe6',
     wolframappid: process.env.wolframappid || ''
 
 };
