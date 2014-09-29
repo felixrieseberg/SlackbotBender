380ee0b765d50e912087a45dc9f813a21218d76f
Felix Rieseberg <felix@felixrieseberg.com>
2014-09-29 10:56:48 +0200

    Test: Update Yell

--- test/yell.js
+++ test/yell.js
@@ -1,13 +1,13 @@
-var should = require('should'),
-    yell = require('../integrations/yell');
+var should  = require('should'),
+    yell    = require('../integrations/yell');
 
 describe('Yell', function(){
-  describe('#getResponse()', function(){
-    it('should return some text when called with a string', function(done){
-    	yell.getResponse('yell hello world!', function(data) { 
-			should.exist(data);
-			done();
-		});
+    describe('#getResponse()', function(){
+        it('should get a response', function (done){
+            yell.getResponse('yell hello world!', function (data) { 
+                should.exist(data);
+                done();
+            });
+        });
     });
-  });
 });
