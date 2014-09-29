164d376657f58178272e1b9c86bfd67db37a9334
Felix Rieseberg <felix@felixrieseberg.com>
2014-09-29 11:08:15 +0200

    Travis Investigation II

--- test/yell.js
+++ test/yell.js
@@ -5,6 +5,7 @@ describe('Yell', function(){
     describe('#getResponse()', function(){
         it('should get a response', function (done){
             yell.getResponse('yell hello world!', function (data) { 
+                console.log(data);
                 should.exist(data);
                 done();
             });
