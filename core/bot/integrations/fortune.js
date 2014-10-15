beeab39dda5021da792fc0f479320359f4d70780
Ivan R. Judson <irjudson@gmail.com>
2014-10-14 17:46:41 -0700

    Fixed image crap

--- core/bot/integrations/fortune.js
+++ core/bot/integrations/fortune.js
@@ -8,7 +8,19 @@ var _       = require('underscore'),
 
 var fortune = {
     getResponse: function (query, callback) {
-        var url = 'http://anduin.eldar.org/cgi-bin/fortune.pl';
+        // There are a number of arguments which might prove to be useful:
+
+        // fortune_db=[fortunes,fortunes2,limerick,pratchett,startrek,unamerican,zippy,bofh-excuses,futurama,farber,netbsd]
+        // long_fortune=yes
+        // offensive_fortune=yes
+        // any_fortune=yes
+        // foreground=[while,red,green,blue,yellow,black]
+        // background=[while,red,green,blue,yellow,black,transparent]
+        // text_format=yes
+        // preformat_text=yes
+        // max_lines=[2 to 999]
+
+        var url = 'http://anduin.eldar.org/cgi-bin/fortune.pl?text_format=yes&preformat_text=yes&any_fortune=yes';
         request.get(url, function (err, res, body) {
             if (err || res.statusCode !== 200) {
                 debug('Fortune error fetching ' + url + ': ' + err + ', ' + res.statusCode);
