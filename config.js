b0297afcfd7b3d7b7ae9ad117430374a6077f11d
Mike Felder <mike.felder@parivedasolutions.com>
2015-04-16 16:03:08 -0500

    actually placed Wolfram Alpha API token in the correct place

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ var config = {
 
     slacktoken: process.env.slacktoken,
     twilioNumber: process.env.twilioNumber || '+123456789',
-    twilioAccount: process.env.twilioAccount || 'VAJ5LG-LAPQPPVVQL',
+    twilioAccount: process.env.twilioAccount || 'VAJ5LG-7QYG7Y9UL4',
     twilioToken: process.env.twilioToken || 'YOURTOKEN',
-    wolframappid: process.env.wolframappid || '',
+    wolframappid: process.env.wolframappid || 'VAJ5LG-7QYG7Y9UL4',
     vsoUser: process.env.vsoUser || 'feriese@microsoft.com',
     vsoPassword: process.env.vsoPassword || '2l6qVviQ',
     vsoCollection: process.env.vsoCollection || 'DefaultCollection',
